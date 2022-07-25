c7961ae7b6efd2abdf5cf58ff07f142f552de369
YoonDongGyun <ydg8732@naver.com>
2022-07-25 21:10:22 +0900

    백준 1008번 A/B 문제 구현

---
+++ src/operations/BJ_1008.java
@@ -0,0 +1,15 @@
+package operations;
+
+import java.util.Scanner;
+
+public class BJ_1008 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		System.out.println(1.0*a/b);
+	}
+
+}
