d6e3279f08b8dfe438b3112e9a39cc9f0ed74bd1
YoonDongGyun <ydg8732@naver.com>
2022-07-25 21:27:21 +0900

    백준 문제 구현

---
+++ src/operations/BJ_25083.java
@@ -0,0 +1,14 @@
+package operations;
+
+public class BJ_25083 {
+
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7\n" + 
+				"r`-_   ,'  ,/\n" + 
+				" \\. \". L_r'\n" + 
+				"   `~\\/\n" + 
+				"      |\n" + 
+				"      |");
+	}
+
+}
