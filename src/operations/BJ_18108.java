c53a47000a15c2323665f9b5c00ac22e5515eb5c
YoonDongGyun <ydg8732@naver.com>
2022-07-25 21:18:48 +0900

    백준 18108 문제 구현

---
+++ src/operations/BJ_18108.java
@@ -0,0 +1,16 @@
+package operations;
+
+import java.util.Scanner;
+
+public class BJ_18108 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int y=sc.nextInt();
+		
+		System.out.println(y-543);
+		
+	}
+
+}
